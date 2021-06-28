017726a38cd5a57b257825e9e179a2e63aa48fe6
MusabMahmoodh <43021789+MusabMahmoodh@users.noreply.github.com>
2021-06-28 23:03:40 +0530

    intial mikroorm setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log("Doner 2")
+import { MikroORM } from '@mikro-orm/core'
+import { __prod__ } from './constants';
+import { Post } from './entities/Posts';
+import mikroOrmConfig from './mikro-orm.config';
+
+const main = async () =>{
+    const orm = await MikroORM.init(mikroOrmConfig);
+    await orm.getMigrator().up()
+    const post = orm.em.create(Post,{title:"my first title"})
+    await orm.em.persistAndFlush(post)
+    
+}
+
+main()
+//j
